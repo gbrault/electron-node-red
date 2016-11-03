f45dcecf0c98da0be2c9ca60c84f95c43ecb67c4
Gilbert Brault <gilbert.brault@hotmail.com>
2016-11-03 16:21:39 +0100

    increased first page load to enable node-red startup
    see line 99 change 500 to 10000 (1/2 a second to 10 seconds)

--- main.js
+++ main.js
@@ -96,7 +96,7 @@ function createWindow () {
     });
 
     // load the initial page
-    setTimeout(function() {mainWindow.loadURL(url)}, 500);
+    setTimeout(function() {mainWindow.loadURL(url)}, 10000);
 
     // Open the DevTools.
     //mainWindow.webContents.openDevTools();
